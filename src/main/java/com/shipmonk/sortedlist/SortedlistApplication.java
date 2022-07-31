f675cfc9beb4c18af6390ebdaf32039a97abc771
jorge98_ <jorgerojas@seti.com.co>
2022-07-30 21:42:14 -0500

    Initial load of sortedlist service

---
+++ src/main/java/com/shipmonk/sortedlist/SortedlistApplication.java
@@ -0,0 +1,13 @@
+package com.shipmonk.sortedlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortedlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortedlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shipmonk/sortedlist/SortedlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipmonk.sortedlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortedlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
